1a01b521ae1777c7e6ead499a796528cf42d20a3
jordan ryan reuter <reuter@cockroachlabs.com>
2022-02-27 23:00:45 -0500

    clean up main.go

--- main.go
+++ main.go
@@ -9,26 +9,22 @@ import (
 	"time"
 
 	"github.com/jreut/pager/opsgenie"
-	"github.com/opsgenie/opsgenie-go-sdk-v2/client"
 )
-
-var cfg client.Config
 
 func main() {
 	log.SetFlags(log.Lshortfile | log.LstdFlags)
 	key := flag.String("key", "", "OpsGenie integration API key")
 	flag.Parse()
-	cfg.ApiKey = *key
 
 	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt)
 	defer cancel()
-	if err := run(ctx, *key); err != nil {
+	if err := og(ctx, *key); err != nil {
 		log.Fatalf("%+v", err)
 	}
 	log.Println("ok")
 }
 
-func run(ctx context.Context, key string) error {
+func og(ctx context.Context, key string) error {
 	client, err := opsgenie.NewClient(key)
 	if err != nil {
 		return err
