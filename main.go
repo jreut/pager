d2599a44b4f0a0429addefa742bbfb4972be31bf
jordan ryan reuter <reuter@cockroachlabs.com>
2022-02-27 23:21:23 -0500

    use the schedule builder
    	go run . generate 2>&1
    	2022/02/27 23:22:00 main.go:75: {Schedule:[jason@2022-02-28T12:00:00-05:00 reuter@2022-03-04T12:00:00-05:00 josh@2022-03-07T12:00:00-05:00 carp@2022-03-11T12:00:00-05:00 jason@2022-03-14T12:00:00-04:00 darius@2022-03-18T12:00:00-04:00 joel@2022-03-21T12:00:00-04:00 reuter@2022-03-25T12:00:00-04:00 carp@2022-03-28T12:00:00-04:00 logston@2022-04-01T12:00:00-04:00 reuter@2022-04-04T12:00:00-04:00 darius@2022-04-08T12:00:00-04:00 josh@2022-04-11T12:00:00-04:00 jason@2022-04-15T12:00:00-04:00 joel@2022-04-18T12:00:00-04:00 logston@2022-04-22T12:00:00-04:00 darius@2022-04-25T12:00:00-04:00 carp@2022-04-29T12:00:00-04:00 jason@2022-05-02T12:00:00-04:00 reuter@2022-05-06T12:00:00-04:00 josh@2022-05-09T12:00:00-04:00 joel@2022-05-13T12:00:00-04:00 logston@2022-05-16T12:00:00-04:00 carp@2022-05-20T12:00:00-04:00 reuter@2022-05-23T12:00:00-04:00 darius@2022-05-27T12:00:00-04:00 jason@2022-05-30T12:00:00-04:00 josh@2022-06-03T12:00:00-04:00 joel@2022-06-06T12:00:00-04:00 carp@2022-06-10T12:00:00-04:00 darius@2022-06-13T12:00:00-04:00 reuter@2022-06-17T12:00:00-04:00 josh@2022-06-20T12:00:00-04:00 jason@2022-06-24T12:00:00-04:00 joel@2022-06-27T12:00:00-04:00 logston@2022-07-01T12:00:00-04:00 carp@2022-07-04T12:00:00-04:00 logston@2022-07-08T12:00:00-04:00 reuter@2022-07-11T12:00:00-04:00 darius@2022-07-15T12:00:00-04:00 jason@2022-07-18T12:00:00-04:00] Balance:map[carp:479h0m0s darius:486h0m0s jason:528h0m0s joel:468h0m0s josh:438h0m0s logston:456h0m0s reuter:504h0m0s]}

--- main.go
+++ main.go
@@ -9,19 +9,31 @@ import (
 	"time"
 
 	"github.com/jreut/pager/opsgenie"
+	"github.com/jreut/pager/schedule"
 )
 
 func main() {
 	log.SetFlags(log.Lshortfile | log.LstdFlags)
-	key := flag.String("key", "", "OpsGenie integration API key")
-	flag.Parse()
 
-	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt)
-	defer cancel()
-	if err := og(ctx, *key); err != nil {
-		log.Fatalf("%+v", err)
+	if len(os.Args) < 2 {
+		log.Fatalf("need a command")
 	}
-	log.Println("ok")
+	switch os.Args[1] {
+	case "override":
+		key := flag.String("key", "", "OpsGenie integration API key")
+		flag.CommandLine.Parse(os.Args[2:])
+		flag.Parse()
+
+		ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt)
+		defer cancel()
+		if err := og(ctx, *key); err != nil {
+			log.Fatalf("%+v", err)
+		}
+		log.Println("ok")
+	case "generate":
+		generate()
+	}
+
 }
 
 func og(ctx context.Context, key string) error {
@@ -37,4 +49,28 @@ func og(ctx context.Context, key string) error {
 		return err
 	}
 	return nil
+}
+
+func generate() {
+	res := schedule.Builder{
+		Interval: schedule.Interval{
+			Time:     time.Date(2022, 02, 28, 12, 0, 0, 0, schedule.NYC),
+			Duration: 20 * 7 * 24 * time.Hour,
+		},
+		Next: schedule.MondayFridayShifts,
+		Balance: schedule.Balance{
+			"carp":    0,
+			"darius":  6 * time.Hour,
+			"jason":   -4 * 24 * time.Hour,
+			"joel":    12 * time.Hour,
+			"josh":    -18 * time.Hour,
+			"logston": 3 * 24 * time.Hour,
+			"reuter":  -3 * 24 * time.Hour,
+		},
+		Exclusions: []schedule.Exclusion{
+			schedule.Exclude("reuter", time.Date(2022, 3, 18, 0, 0, 0, 0, schedule.NYC), time.Date(2022, 3, 22, 0, 0, 0, 0, schedule.NYC)),
+			schedule.Exclude("logston", time.Date(2022, 6, 1, 0, 0, 0, 0, time.UTC), time.Date(2022, 7, 1, 0, 0, 0, 0, time.UTC)),
+		},
+	}.Build()
+	log.Printf("%+v", res)
 }
