90fc39ec0e43fd3b7c9bca4d5f1bb0196d64ea95
jordan ryan reuter <reuter@cockroachlabs.com>
2022-02-28 22:03:47 -0500

    add flags to generate command and write to stdout

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"flag"
+	"io"
 	"log"
 	"os"
 	"os/signal"
@@ -22,7 +23,6 @@ func main() {
 	case "override":
 		key := flag.String("key", "", "OpsGenie integration API key")
 		flag.CommandLine.Parse(os.Args[2:])
-		flag.Parse()
 
 		ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt)
 		defer cancel()
@@ -31,7 +31,30 @@ func main() {
 		}
 		log.Println("ok")
 	case "generate":
-		generate()
+		from := flag.String("from", time.Now().Format(time.RFC3339), "RFC3339-formatted time to start generating the schedule")
+		dur := flag.Duration("for", 28*2*24*time.Hour, "length of time to generate")
+		flag.CommandLine.Parse(os.Args[2:])
+		start, err := time.Parse(time.RFC3339, *from)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		if err := generate(os.Stdout, schedule.Interval{
+			Time: start, Duration: *dur,
+		}, schedule.Balance{ // generate the balance from a recorded starting point and then by processing the schedule up to this point
+			"carp":    0,
+			"darius":  6 * time.Hour,
+			"jason":   -4 * 24 * time.Hour,
+			"joel":    12 * time.Hour,
+			"josh":    -18 * time.Hour,
+			"logston": 3 * 24 * time.Hour,
+			"reuter":  -3 * 24 * time.Hour,
+		}, []schedule.Exclusion{ // read the exclusions from the filesystem
+			schedule.Exclude("reuter", time.Date(2022, 3, 18, 0, 0, 0, 0, schedule.NYC), time.Date(2022, 3, 22, 0, 0, 0, 0, schedule.NYC)),
+			schedule.Exclude("logston", time.Date(2022, 6, 1, 0, 0, 0, 0, time.UTC), time.Date(2022, 7, 1, 0, 0, 0, 0, time.UTC)),
+		}); err != nil {
+			log.Fatalf("%+v", err)
+		}
 	}
 
 }
@@ -51,26 +74,12 @@ func og(ctx context.Context, key string) error {
 	return nil
 }
 
-func generate() {
+func generate(w io.Writer, i schedule.Interval, b schedule.Balance, es []schedule.Exclusion) error {
 	res := schedule.Builder{
-		Interval: schedule.Interval{
-			Time:     time.Date(2022, 02, 28, 12, 0, 0, 0, schedule.NYC),
-			Duration: 20 * 7 * 24 * time.Hour,
-		},
-		Next: schedule.MondayFridayShifts,
-		Balance: schedule.Balance{
-			"carp":    0,
-			"darius":  6 * time.Hour,
-			"jason":   -4 * 24 * time.Hour,
-			"joel":    12 * time.Hour,
-			"josh":    -18 * time.Hour,
-			"logston": 3 * 24 * time.Hour,
-			"reuter":  -3 * 24 * time.Hour,
-		},
-		Exclusions: []schedule.Exclusion{
-			schedule.Exclude("reuter", time.Date(2022, 3, 18, 0, 0, 0, 0, schedule.NYC), time.Date(2022, 3, 22, 0, 0, 0, 0, schedule.NYC)),
-			schedule.Exclude("logston", time.Date(2022, 6, 1, 0, 0, 0, 0, time.UTC), time.Date(2022, 7, 1, 0, 0, 0, 0, time.UTC)),
-		},
+		Interval:   i,
+		Next:       schedule.MondayFridayShifts,
+		Balance:    b,
+		Exclusions: es,
 	}.Build()
-	log.Printf("%+v", res)
+	return res.Schedule.WriteCSV(w)
 }
