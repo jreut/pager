88d3ba11edcc7da20a2890760cad002fbae62fb1
jordan ryan reuter <pub@aviary.biz>
2022-10-31 22:52:20 -0400

    wip

--- pkg/shift/shift.go
+++ pkg/shift/shift.go
@@ -1,30 +1,68 @@
 package shift
 
 import (
+	"fmt"
 	"sort"
 
 	"github.com/jreut/pager/v2/pkg/save"
 )
 
-func Flatten(in []save.Shift) ([]save.Shift, error) {
-	var out []save.Shift
-	for i := range in {
-		var in2 []save.Shift
-		n := copy(in2, in[i:])
-		if n == 0 {
-			break
-		}
-		sort.Slice(in2, func(i, j int) bool {
-			t_i, t_j := in2[i].StartAt, in2[j].StartAt
-			switch {
-			case t_i.Before(t_j):
-				return true
-			case t_i.Equal(t_j):
-				return i < j
-			default:
-				return false
-			}
-		})
+func Flatten(in []save.Shift) []save.Shift {
+	if len(in) < 2 {
+		return in
 	}
-	return out, nil
+	out := []save.Shift{in[0]}
+	for _, i := range in[1:] {
+		i_start, i_end := i.StartAt, i.EndBefore
+		for j := range out {
+			j_start, j_end := out[j].StartAt, out[j].EndBefore
+			// j: alice: [-------)
+			// i:   bob:   [---)
+			// -------------------
+			//    alice: [-)   [-)
+			//      bob:   [---)
+			if j_start.Before(i_start) && j_end.After(i_end) {
+				left := save.Shift{Person: out[j].Person, StartAt: out[j].StartAt, EndBefore: i.StartAt}
+				right := save.Shift{Person: out[j].Person, StartAt: i.EndBefore, EndBefore: out[j].EndBefore}
+				out = append(append(out[:j], left, i, right), out[j+1:]...)
+				break
+			}
+			// j:   bob:   [---)
+			// i: alice: [-------)
+			// -------------------
+			//    alice: [-------)
+			if !j_start.Before(i_start) && !j_end.After(i_end) {
+				out = append(append(out[:j], i), out[j+1:]...)
+				break
+			}
+		}
+	}
+	return out
+}
+
+func sortshifts(in []save.Shift) {
+	sort.Slice(in, func(i, j int) bool {
+		if overlap(in[i], in[j]) {
+			return i < j
+		}
+		return in[i].StartAt.Before(in[j].StartAt)
+	})
+}
+
+func overlap(a, b save.Shift) bool {
+	a_start, a_end := a.StartAt, a.EndBefore
+	b_start, b_end := b.StartAt, b.EndBefore
+	if a_start.After(a_end) {
+		panic(fmt.Sprintf("invalid shift: start > end: %+v", a))
+	}
+	if b_start.After(b_end) {
+		panic(fmt.Sprintf("invalid shift: start > end: %+v", b))
+	}
+	if !a_end.After(b_start) {
+		return false
+	}
+	if !b_end.After(a_start) {
+		return false
+	}
+	return true
 }
--- pkg/shift/shift_test.go
+++ pkg/shift/shift_test.go
@@ -1,4 +1,4 @@
-package shift_test
+package shift
 
 import (
 	"testing"
@@ -6,23 +6,27 @@ import (
 
 	"github.com/jreut/pager/v2/pkg/assert"
 	"github.com/jreut/pager/v2/pkg/save"
-	"github.com/jreut/pager/v2/pkg/shift"
+)
+
+const (
+	alice = "alice"
+	bob   = "bob"
+	cindy = "cindy"
+	dana  = "dana"
+)
+
+var (
+	t0 = time.Unix(0, 0).In(time.UTC)
+	t1 = t0.Add(1 * time.Minute)
+	t2 = t0.Add(2 * time.Minute)
+	t3 = t0.Add(3 * time.Minute)
+	t4 = t0.Add(4 * time.Minute)
+	t5 = t0.Add(5 * time.Minute)
 )
 
 func TestFlatten(t *testing.T) {
-	const (
-		alice = "alice"
-		bob   = "bob"
-	)
-	var (
-		t0 = time.Unix(0, 0)
-		t1 = t0.AddDate(0, 0, 1)
-		t2 = t0.AddDate(0, 0, 2)
-		t3 = t0.AddDate(0, 0, 3)
-	)
 	for _, tt := range []struct {
 		in, want []save.Shift
-		err      string
 	}{
 		{},
 		{
@@ -35,11 +39,23 @@ func TestFlatten(t *testing.T) {
 		},
 		{
 			in: []save.Shift{
-				{Person: alice, StartAt: t0, EndBefore: t1},
-				{Person: alice, StartAt: t1, EndBefore: t2},
+				{Person: alice, StartAt: t0, EndBefore: t3},
+				{Person: bob, StartAt: t1, EndBefore: t2},
 			},
 			want: []save.Shift{
-				{Person: alice, StartAt: t0, EndBefore: t2},
+				{Person: alice, StartAt: t0, EndBefore: t1},
+				{Person: bob, StartAt: t1, EndBefore: t2},
+				{Person: alice, StartAt: t2, EndBefore: t3},
+			},
+		},
+		{
+			in: []save.Shift{
+				{Person: bob, StartAt: t1, EndBefore: t2},
+				{Person: cindy, StartAt: t0, EndBefore: t3},
+				{Person: alice, StartAt: t0, EndBefore: t3},
+			},
+			want: []save.Shift{
+				{Person: alice, StartAt: t0, EndBefore: t3},
 			},
 		},
 		{
@@ -52,32 +68,146 @@ func TestFlatten(t *testing.T) {
 				{Person: bob, StartAt: t1, EndBefore: t3},
 			},
 		},
+		// {
+		// 	in: []save.Shift{
+		// 		{Person: alice, StartAt: t0, EndBefore: t1},
+		// 		{Person: alice, StartAt: t1, EndBefore: t2},
+		// 	},
+		// 	want: []save.Shift{
+		// 		{Person: alice, StartAt: t0, EndBefore: t2},
+		// 	},
+		// },
+		// {
+		// 	in: []save.Shift{
+		// 		{Person: alice, StartAt: t0, EndBefore: t2},
+		// 		{Person: bob, StartAt: t1, EndBefore: t3},
+		// 	},
+		// 	want: []save.Shift{
+		// 		{Person: alice, StartAt: t0, EndBefore: t1},
+		// 		{Person: bob, StartAt: t1, EndBefore: t3},
+		// 	},
+		// },
+		// {
+		// 	in: []save.Shift{
+		// 		{Person: bob, StartAt: t1, EndBefore: t3},
+		// 		{Person: alice, StartAt: t0, EndBefore: t2},
+		// 	},
+		// 	want: []save.Shift{
+		// 		{Person: alice, StartAt: t0, EndBefore: t2},
+		// 		{Person: bob, StartAt: t2, EndBefore: t3},
+		// 	},
+		// },
+		// {
+		// 	in: []save.Shift{
+		// 		{Person: alice, StartAt: t0, EndBefore: t2},
+		// 		{Person: bob, StartAt: t2, EndBefore: t3},
+		// 		{Person: cindy, StartAt: t3, EndBefore: t5},
+		// 		{Person: dana, StartAt: t1, EndBefore: t4},
+		// 	},
+		// 	want: []save.Shift{
+		// 		{Person: alice, StartAt: t0, EndBefore: t1},
+		// 		{Person: dana, StartAt: t1, EndBefore: t4},
+		// 		{Person: cindy, StartAt: t4, EndBefore: t5},
+		// 	},
+		// },
+		// {
+		// 	in: []save.Shift{
+		// 		{Person: alice, StartAt: t0, EndBefore: t1},
+		// 		{Person: bob, StartAt: t2, EndBefore: t3},
+		// 	},
+		// },
+	} {
+		t.Run("", func(t *testing.T) {
+			got := Flatten(tt.in)
+			assert.Cmp(t, tt.want, got)
+		})
+	}
+}
+
+func TestSortShifts(t *testing.T) {
+	for _, tt := range []struct{ in, want []save.Shift }{
 		{
 			in: []save.Shift{
-				{Person: bob, StartAt: t1, EndBefore: t3},
-				{Person: alice, StartAt: t0, EndBefore: t2},
+				{Person: alice, StartAt: t0, EndBefore: t1},
+				{Person: bob, StartAt: t1, EndBefore: t2},
+				{Person: cindy, StartAt: t2, EndBefore: t3},
 			},
 			want: []save.Shift{
-				{Person: alice, StartAt: t0, EndBefore: t2},
-				{Person: bob, StartAt: t2, EndBefore: t3},
+				{Person: alice, StartAt: t0, EndBefore: t1},
+				{Person: bob, StartAt: t1, EndBefore: t2},
+				{Person: cindy, StartAt: t2, EndBefore: t3},
 			},
 		},
 		{
 			in: []save.Shift{
+				{Person: bob, StartAt: t1, EndBefore: t2},
 				{Person: alice, StartAt: t0, EndBefore: t1},
-				{Person: bob, StartAt: t2, EndBefore: t3},
+				{Person: cindy, StartAt: t2, EndBefore: t3},
 			},
-			err: "gap",
+			want: []save.Shift{
+				{Person: alice, StartAt: t0, EndBefore: t1},
+				{Person: bob, StartAt: t1, EndBefore: t2},
+				{Person: cindy, StartAt: t2, EndBefore: t3},
+			},
+		},
+		{
+			in: []save.Shift{
+				{Person: alice, StartAt: t2, EndBefore: t5},
+				{Person: bob, StartAt: t1, EndBefore: t3},
+				{Person: cindy, StartAt: t0, EndBefore: t2},
+			},
+			want: []save.Shift{
+				{Person: alice, StartAt: t2, EndBefore: t5},
+				{Person: bob, StartAt: t1, EndBefore: t3},
+				{Person: cindy, StartAt: t0, EndBefore: t2},
+			},
 		},
 	} {
 		t.Run("", func(t *testing.T) {
-			got, err := shift.Flatten(tt.in)
+			got := make([]save.Shift, len(tt.in))
+			copy(got, tt.in)
+			sortshifts(got)
 			assert.Cmp(t, tt.want, got)
-			if tt.err == "" {
-				assert.Nil(t, err)
-			} else {
-				assert.Error(t, tt.err, err)
-			}
+		})
+	}
+}
+
+func TestOverlap(t *testing.T) {
+	for _, tt := range []struct {
+		a, b save.Shift
+		want bool
+	}{
+		{
+			a:    save.Shift{StartAt: t0, EndBefore: t1},
+			b:    save.Shift{StartAt: t1, EndBefore: t2},
+			want: false,
+		},
+		{
+			a:    save.Shift{StartAt: t0, EndBefore: t1},
+			b:    save.Shift{StartAt: t0, EndBefore: t2},
+			want: true,
+		},
+		{
+			a:    save.Shift{StartAt: t0, EndBefore: t2},
+			b:    save.Shift{StartAt: t1, EndBefore: t3},
+			want: true,
+		},
+		{
+			a:    save.Shift{StartAt: t0, EndBefore: t1},
+			b:    save.Shift{StartAt: t2, EndBefore: t3},
+			want: false,
+		},
+		{
+			a:    save.Shift{StartAt: t0, EndBefore: t1},
+			b:    save.Shift{StartAt: t0, EndBefore: t1},
+			want: true,
+		},
+	} {
+		t.Run("", func(t *testing.T) {
+			got := overlap(tt.a, tt.b)
+			assert.Cmp(t, tt.want, got)
+			got2 := overlap(tt.b, tt.a)
+			assert.Cmp(t, tt.want, got2)
 		})
 	}
 }
